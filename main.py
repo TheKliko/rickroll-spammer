3548179e46b140f9dd98b689f335bbe31920ed5e
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-06-11 12:37:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ URL: str = r'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 
 def main() -> None:
-    for i in range(15):
+    while True:
         webbrowser.open(url=URL, new=2, autoraise=True)
 
 
